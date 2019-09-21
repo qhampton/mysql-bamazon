4392e012ae61c800dcecdd61c3af3f76518aada6
Quinlan <qnhampton93@gmail.com>
2019-09-21 10:38:50 -0700

    adding recovered full schema and seeds files, full file set up

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+USE bamazon_DB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+	("Max Revive", "Medicine", 100, 1000),
+    ("Potions", "Medicine", 10, 95),
+    ("Poke Ball", "Technology", 15, 150000),
+    ("Master Ball", "Technology", 25, 100),
+    ("Poke Flute ", "Music", 3, 100),
+    ("Poke Chow", "Food", 15, 150),
+    ("Rope", "Outdoors", 20, 26),
+    ("Antidote", "Medicine", 10, 300),
+    ("Ultra Ball", "Technology", 45, 1000),
+    ("Lighting Stone", "Magic", 15000, 45);
